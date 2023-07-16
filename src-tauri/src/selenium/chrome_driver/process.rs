598ea7565dfbc584611267f646dc8ac320ef2779
callasio <callasio39@gmail.com>
2023-07-17 03:06:16 +0900

    Remove termination functionality for `ChromeDriver` process

--- src-tauri/src/selenium/chrome_driver/process.rs
+++ src-tauri/src/selenium/chrome_driver/process.rs
@@ -4,28 +4,11 @@ use super::error::ExecutionError;
 impl super::ChromeDriver {
     pub async fn execute(&self) -> Result<(), ExecutionError> {
         let mut command = process::Command::new(&self.execution_file_path);
+        command.kill_on_drop(true);
 
         let mut process_guard = self.execution_process.lock().await;
         *process_guard = Some(command.spawn()?);
 
         Ok(())
-    }
-
-    async fn terminate(&mut self) {
-        if let Some(child_process) = &mut *self.execution_process.lock().await {
-            child_process.kill()
-                .await
-                .unwrap_or_else(
-                    |err| eprintln!("Failed to kill chromedriver process. Error: {}", err)
-                );
-        }
-    }
-}
-
-impl Drop for super::ChromeDriver {
-    fn drop(&mut self) {
-        futures::executor::block_on(async {
-            self.terminate().await;
-        });
     }
 }
